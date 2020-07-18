713c3b33345ba3249d430d672164244839c0aa49
zjschulz <zjschulz@gmail.com>
2020-07-18 00:49:10 +0000

    organized navbar and app, downloaded router

---
+++ website-frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default class App extends React.Component {
+  
+  render () {
+
+    return (
+      <div className="App">
+        <Navbar/>
+      </div>
+    );
+
+  }
+  
+}
