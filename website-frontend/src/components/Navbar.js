e05a85cc2b01cd86ed9cc9da36a3532d3db69334
zjschulz <zjschulz@gmail.com>
2020-08-23 21:11:22 +0000

    add styling to navbar and add bootstrap.css

--- website-frontend/src/components/Navbar.js
+++ website-frontend/src/components/Navbar.js
@@ -4,14 +4,15 @@ export default class Navbar extends React.Component {
 
   render() {
     return (
-      <nav className="Navbar">
-        <h1>Navbar</h1>
-        <ul>
-            <li>AboutMe</li>
-            <li>Projects</li>
-            <li>Resume</li>
-            <li>Social Media</li>
-        </ul>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link" href="#">AboutMe</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Resume</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Social Media</a></li>
+          </ul>
+        </div>
       </nav>
     );
   }
--- website-frontend/src/index.js
+++ website-frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import "/home/zjschulz/code/website/website-frontend/src/style/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
