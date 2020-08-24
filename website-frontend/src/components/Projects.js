2962f2d995d6e65c06b161aca47db20636761a6b
zjschulz <zjschulz@gmail.com>
2020-08-24 18:54:41 +0000

    create horizontal responsive image gallery for projects

--- website-frontend/src/components/Projects.js
+++ website-frontend/src/components/Projects.js
@@ -6,12 +6,34 @@ export default class Projects extends React.Component {
     return (
         <div className="Projects" id="Projects">
             <h1>Projects</h1>
-            <ul>
-                <li>Try Awarded</li>
-                <li>Yardsale</li>
-                <li>Dog App</li>
-                <li>Dungeon Master Tool</li>
-            </ul>
+            <div class="accordion">
+              <ul>
+                <li>
+                  <div> <a href="#">
+                    <h2>Try Awarded</h2>
+                    <p>Description 1</p>
+                    </a> </div>
+                </li>
+                <li>
+                  <div> <a href="#">
+                    <h2>Yardsale</h2>
+                    <p>Description 2</p>
+                    </a> </div>
+                </li>
+                <li>
+                  <div> <a href="#">
+                    <h2>Dog Walker</h2>
+                    <p>Description 3</p>
+                    </a> </div>
+                </li>
+                <li>
+                  <div> <a href="#">
+                    <h2>Dungeon Master Tool</h2>
+                    <p>Description 4</p>
+                    </a> </div>
+                </li>
+              </ul>
+            </div>
         </div>
     );
   }
