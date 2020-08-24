3467d66e164fabe7c39c8e1e0e29d8a24f4338ff
zjschulz <zjschulz@gmail.com>
2020-08-24 20:35:38 +0000

    update image links with youtube walkthrough vids

--- website-frontend/src/components/Projects.js
+++ website-frontend/src/components/Projects.js
@@ -5,29 +5,29 @@ export default class Projects extends React.Component {
   render() {
     return (
         <div className="Projects" id="Projects">
-            <h1>Projects</h1>
+            <h1><center>Projects</center></h1>
             <div class="accordion">
               <ul>
                 <li>
-                  <div> <a href="#">
+                  <div> <a href="https://www.youtube.com/watch?v=ZyaNO0lgK18&t=13s">
                     <h2>Try Awarded</h2>
                     <p>Description 1</p>
                     </a> </div>
                 </li>
                 <li>
-                  <div> <a href="#">
+                  <div> <a href="https://www.youtube.com/watch?v=R_NATOJQ8iA">
                     <h2>Yardsale</h2>
                     <p>Description 2</p>
                     </a> </div>
                 </li>
                 <li>
-                  <div> <a href="#">
+                  <div> <a href="https://www.youtube.com/watch?v=ce2nz0LCxm4&t=15s">
                     <h2>Dog Walker</h2>
                     <p>Description 3</p>
                     </a> </div>
                 </li>
                 <li>
-                  <div> <a href="#">
+                  <div> <a href="https://www.youtube.com/watch?v=kaYfgoIQWFQ">
                     <h2>Dungeon Master Tool</h2>
                     <p>Description 4</p>
                     </a> </div>
