b89697ad543fa0a86b9af246d343d7fdc4018375
zjschulz <zjschulz@gmail.com>
2020-08-26 18:54:52 +0000

    add icons and links to social media accounts

--- website-frontend/src/components/SocialMedia.js
+++ website-frontend/src/components/SocialMedia.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import facebook from '../icons8-facebook-50.png';
+import github from '../icons8-github-50.png';
+import insta from '../icons8-instagram-50.png';
+import linkedin from '../icons8-linkedin-50.png';
+import twitter from '../icons8-twitter-50.png';
 
 export default class SocialMedia extends React.Component {
 
@@ -6,9 +11,19 @@ export default class SocialMedia extends React.Component {
     return (
         <div className="SocialMedia" id="SocialMedia">
             <center><h1>Social Media</h1>
-                <p>LinkedIn</p>
-                <p>GitHub</p>
-                <p>Other</p></center>
+                <a href="https://www.linkedin.com/in/zachary-schulz-65946663/" target="_blank">
+                  <img src={linkedin}></img>
+                </a>
+                <a href="https://github.com/zjschulz" target="_blank">
+                  <img src={github}></img>
+                </a>
+                <a href="https://www.facebook.com/zachary.schulz.50" target="_blank">
+                  <img src={facebook}></img>
+                </a>
+                <a href="https://www.instagram.com/zachary.schulz.50/" target="_blank">
+                  <img src={insta}></img>
+                </a>
+                </center>
         </div>
     );
   }
