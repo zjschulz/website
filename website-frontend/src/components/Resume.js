4817da0d3970eae873f49d1e6ccde14fed507681
zjschulz <zjschulz@gmail.com>
2020-08-26 15:09:51 +0000

    upload resume.pdf and display pdf on page

--- website-frontend/src/components/Resume.js
+++ website-frontend/src/components/Resume.js
@@ -5,7 +5,13 @@ export default class Resume extends React.Component {
   render() {
     return (
         <div className="Resume" id="Resume">
-            <center><h1>Resume</h1></center>
+            <center>
+            <h1>Resume</h1>
+            <iframe src="https://docs.google.com/document/d/e/2PACX-1vS0IlemYVvSGmKTwuueBjBY5Cb9sy2mjWDP6YEEpxesysIi2naxuSEKY3ts7Pud_PFt-Qc8FcxRlnX9/pub"
+            type="application/pdf"
+            width="100%"
+            height="600px" />
+            </center>
         </div>
     );
   }
