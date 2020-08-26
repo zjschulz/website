be5ca19e955110699e3a73f2bbf4ea0e8cc0e3c3
zjschulz <zjschulz@gmail.com>
2020-08-26 18:39:22 +0000

    add show/hide toggle buttons for resume

--- website-frontend/src/components/Resume.js
+++ website-frontend/src/components/Resume.js
@@ -3,14 +3,52 @@ import pdf from '../Resume 8.14.20.pdf';
 
 export default class Resume extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        hidden: "yes",
+    }
+
+    this.showPDF = this.showPDF.bind(this);
+    this.hidePDF = this.hidePDF.bind(this);
+    this.renderPDF = this.renderPDF.bind(this);
+}
+
+  showPDF(event) {
+    this.setState({
+      hidden: "no"
+    })
+  }
+
+  hidePDF(event) {
+    this.setState({
+      hidden: "yes"
+    })
+  }
+
+  renderPDF() {
+    if (this.state.hidden === "no") {
+      return (
+        <iframe src={pdf}
+        title="PDF"
+        type="application/pdf"
+        width="75%"
+        height="600px" />
+      )
+    }
+  }
+
   render() {
     return (
         <div className="Resume" id="Resume">
             <center>
             <h1>Resume</h1>
-            <iframe src={pdf} type="application/pdf"
-            width="75%"
-            height="600px" />
+            <button type="button" class="btn btn-secondary" onClick={this.showPDF}>Show</button>
+            <button type="button" class="btn btn-secondary" onClick={this.hidePDF}>Hide</button>
+            <p>
+            {this.renderPDF()}
+            </p>
             </center>
         </div>
     );
