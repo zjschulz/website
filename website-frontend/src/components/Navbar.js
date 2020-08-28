1f56653d2c0275346ad19e0baed868e31e1dae20
zjschulz <zjschulz@gmail.com>
2020-08-28 16:53:32 +0000

    finish build of dropdown menu

--- website-frontend/src/components/Navbar.js
+++ website-frontend/src/components/Navbar.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 export default class Navbar extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.myFunction = this.myFunction.bind(this);
+  }
+
+  myFunction() {
+    const drop = document.getElementById("myDropdown");
+    drop.classList.toggle("show");
+  }
+
   render() {
     return (
       <div>
@@ -10,14 +21,13 @@ export default class Navbar extends React.Component {
           <div class="collapse navbar-collapse" id="navbarColor01" style={{display: "flex"}}>
             <ul class="navbar-nav mr-auto">
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About Me</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
+                <a onClick={this.myFunction} class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About Me</a>
+                <div class="dropdown-menu" id="myDropdown">
+                  <a class="dropdown-item" href="#Education">Education</a>
+                  <a class="dropdown-item" href="#Work">Work Experience</a>
+                  <a class="dropdown-item" href="#Extra">Extra Curriculars</a>
                 </div>
               </li>
-              <li class="nav-item"><a class="nav-link" href="#AboutMe">AboutMe</a></li>
               <li class="nav-item"><a class="nav-link" href="#Projects">Projects</a></li>
               <li class="nav-item"><a class="nav-link" href="#Resume">Resume</a></li>
               <li class="nav-item"><a class="nav-link" href="#SocialMedia">Social Media</a></li>
