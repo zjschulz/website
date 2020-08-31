6bf0913e22bf0ee5e8248cf3495ec77d9b062471
zjschulz <zjschulz@gmail.com>
2020-08-31 17:22:43 +0000

    add toggle off functionality for navbar dropdown

--- website-frontend/src/index.js
+++ website-frontend/src/index.js
@@ -9,3 +9,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-toggle')) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
