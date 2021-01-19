ac1c5c7a4eece5c5e41949c061f4ddfda4725fbc
zjschulz <zjschulz@gmail.com>
2021-01-19 16:12:28 +0000

    change html to match about me changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,7 +9,7 @@ export default class AboutMe extends React.Component {
             <h1>About Me</h1>
             <div class="row" style={{width: '80%'}}>
               <div class="col-md-5">
-                <img src="https://image.shutterstock.com/image-photo/close-portrait-smiling-handsome-man-260nw-1011569245.jpg"></img>
+                <img alt="" src="https://image.shutterstock.com/image-photo/close-portrait-smiling-handsome-man-260nw-1011569245.jpg"></img>
               </div>
               <div class="col-md-7">
                 Full stack web developer with Bachelor’s degree in Physics and background in administrative management. Adept in using programming languages Ruby, JavaScript, HTML, and CSS which includes frameworks like React, Redux, and Ruby on Rails. Seeking a position in which I can utilize my professional and creative skills to benefit your company.
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,14 +20,15 @@ export default class Navbar extends React.Component {
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
           <div className="collapse navbar-collapse" id="navbarColor01" style={{display: "flex", flexWrap: 'wrap', justifyContent: "center", flexDirection: 'row'}}>
             <ul className="navbar-nav mr-auto" style={{flexDirection: 'row'}}>
-              <li className="nav-item dropdown">
+              {/* <li className="nav-item dropdown">
                 <a onClick={this.myFunction} className="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About Me</a>
                 <div className="dropdown-menu" id="myDropdown">
                   <a className="dropdown-item" href="#Education">Education</a>
                   <a className="dropdown-item" href="#Work">Work Experience</a>
                   <a className="dropdown-item" href="#Extra">Extra Curriculars</a>
                 </div>
-              </li>
+              </li> */}
+              <li className="nav-item"><a className="nav-link" href="#AboutMe">About Me</a></li>
               <li className="nav-item"><a className="nav-link" href="#Projects">Projects</a></li>
               <li className="nav-item"><a className="nav-link" href="#Resume">Resume</a></li>
               <li className="nav-item"><a className="nav-link" href="#SocialMedia">Social Media</a></li>
