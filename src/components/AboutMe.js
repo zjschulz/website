cb12399876b84f6b7f36047a08c14ba812a757ac
zjschulz <zjschulz@gmail.com>
2021-02-09 02:04:01 +0000

    update AboutMe with new portrait

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import portrait from '../images/portrait.jpg';
 
 export default class AboutMe extends React.Component {
 
@@ -7,9 +8,9 @@ export default class AboutMe extends React.Component {
         <div className="AboutMe" id="AboutMe">
           <center>
             <h1 style={{backgroundImage: 'linear-gradient(to bottom left, whitesmoke, lightblue)'}}>About Me</h1>
-            <div class="row" style={{width: '80%'}}>
+            <div class="row" style={{width: '80%', marginBottom: '0.5rem'}}>
               <div class="col-md-5">
-                <img alt="" src="https://image.shutterstock.com/image-photo/close-portrait-smiling-handsome-man-260nw-1011569245.jpg"></img>
+                <img alt="" src={portrait} style={{width: '100%'}}></img>
               </div>
               <div class="col-md-7">
                 <div class="row" style={{textAlign: "left"}}>
@@ -17,8 +18,12 @@ export default class AboutMe extends React.Component {
                   Adept in using programming languages Ruby, JavaScript, HTML, and CSS which includes frameworks like React, Redux, Angular, and Ruby on Rails.
                 </div>
                 <p></p>
-                <div class="row">
-                  Maybe put images of my languages here? Or maybe my brief history?
+                <div class="row" style={{textAlign: "left"}}>
+                  Education?
+                </div>
+                <p></p>
+                <div class="row" style={{textAlign: "left"}}>
+                  Other Interests?
                 </div>
               </div>
             </div>
